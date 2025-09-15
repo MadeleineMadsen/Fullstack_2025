91a9ea7f3e452a3f2bce362dd4ccc6e7daf423cd
Madeleine Madsen <mama0002@stud.kea.dk>
2025-09-15 09:56:02 +0200

    Fra undervisningen - Create list of platform icons in game card

--- frontend/src/hooks/useGames.ts
+++ frontend/src/hooks/useGames.ts
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 
 interface Platform {
+    export interface Platform {
     id: number;
     name: string;
     slug: string;

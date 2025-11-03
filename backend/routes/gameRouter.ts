627c0b889b3a0a4ee460002e208fdd61d8b1d356
Madeleine Madsen <mama0002@stud.kea.dk>
2025-11-03 12:10:39 +0100

    Fra undervisningen - Implemented pagination on the server side

--- backend/routes/gameRouter.ts
+++ backend/routes/gameRouter.ts
@@ -1,144 +1,59 @@
 import { Router } from "express";
-import { SelectQueryBuilder } from "typeorm";
-import { AppDataSource } from "../data-source";
 import { Game } from "../entities/Game";
 import { ParentPlatform } from "../entities/ParentPlatform";
+import { getGames } from "../services/gameService";
 
-type ModifiedGame = Omit<Game, "parent_platforms"> & {
+export type ModifiedGame = Omit<Game, "parent_platforms"> & {
   parent_platforms: { platform: ParentPlatform }[];
 };
 
+export const DEFAULT_PAGE_SIZE = 20;
+export const START_PAGE = 1;
+export const MAX_PAGE_SIZE = 40;
+
 interface Response {
   count: number;
+  next: string | null;
   results: ModifiedGame[];
 }
 
 const gameRouter = Router();
-const gameRepository = AppDataSource.getRepository(Game);
 
-const addGenreFilter = (
-  queryBuilder: SelectQueryBuilder<Game>,
-  genreId: number | undefined
-) => {
-  if (genreId) {
-    queryBuilder
-      .andWhere((qb) => {
-        const subQuery = qb
-          .subQuery()
-          .select("game.id")
-          .from(Game, "game")
-          .leftJoin("game.genres", "genres")
-          .where("genres.id = :genreId")
-          .getQuery();
-        return "game.id IN " + subQuery;
-      })
-      .setParameter("genreId", genreId);
+const buildGamesResponse = (
+  games: ModifiedGame[],
+  total: number,
+  req: any
+): Response => {
+  const page = req.query.page ? Number(req.query.page) : START_PAGE;
+
+  let pageSize = req.query.page_size
+    ? Number(req.query.page_size)
+    : DEFAULT_PAGE_SIZE;
+
+  if (pageSize > MAX_PAGE_SIZE) {
+    pageSize = MAX_PAGE_SIZE;
   }
+
+  const totalPages = Math.ceil(total / pageSize);
+
+  return {
+    count: total,
+    next:
+      page < totalPages
+        ? `${process.env.SERVER_URL}/games?page=${
+            page + 1
+          }&page_size=${pageSize}`
+        : null,
+    results: games,
+  };
 };
 
-const addGenrefilterNativeQuery = (
-  queryBuilder: SelectQueryBuilder<Game>,
-  genreId: number | undefined
-) => {
-  if (genreId) {
-    queryBuilder.andWhere(
-      "game.id IN (SELECT games_id FROM games_has_genres WHERE genres_id = :genreId)",
-      { genreId }
-    );
-  }
-};
-
-const addStoreFilter = (
-  queryBuilder: SelectQueryBuilder<Game>,
-  storeId: number | undefined
-) => {
-  if (storeId) {
-    queryBuilder
-      .andWhere((qb) => {
-        const subQuery = qb
-          .subQuery()
-          .select("game.id")
-          .from(Game, "game")
-          .leftJoin("game.stores", "stores")
-          .where("stores.id = :storeId")
-          .getQuery();
-        return "game.id IN " + subQuery;
-      })
-      .setParameter("storeId", storeId);
-  }
-};
-
-const addParentPlatformFilter = (
-  queryBuilder: SelectQueryBuilder<Game>,
-  parentPlatformId: number | undefined
-) => {
-  if (parentPlatformId) {
-    queryBuilder
-      .andWhere((qb) => {
-        const subQuery = qb
-          .subQuery()
-          .select("game.id")
-          .from(Game, "game")
-          .leftJoin("game.parent_platforms", "parent_platforms")
-          .where("parent_platforms.id = :parentPlatformId")
-          .getQuery();
-        return "game.id IN " + subQuery;
-      })
-      .setParameter("parentPlatformId", parentPlatformId);
-  }
-};
-
-const addOrdering = (
-  queryBuilder: SelectQueryBuilder<Game>,
-  ordering: string | undefined
-) => {
-  //TODO: implement proper relevance ordering
-  if (!ordering) queryBuilder.orderBy("game.rating", "DESC"); //default relevance ordering
-  if (ordering === "-added") queryBuilder.orderBy("game.added", "DESC");
-  if (ordering === "name") queryBuilder.orderBy("game.name", "ASC");
-  if (ordering === "-released") queryBuilder.orderBy("game.released", "DESC");
-  if (ordering === "metacritic")
-    queryBuilder.orderBy("game.metacritic", "DESC");
-  if (ordering === "-rating") queryBuilder.orderBy("game.rating", "DESC");
-};
 gameRouter.get("/", async (req, res) => {
-  const genreId = req.query.genres ? Number(req.query.genres) : undefined;
-  const storeId = req.query.stores ? Number(req.query.stores) : undefined;
-  const parentPlatformId = req.query.platforms
-    ? Number(req.query.platforms)
-    : undefined;
-  const ordering = req.query.ordering ? String(req.query.ordering) : undefined;
-  try {
-    // Using QueryBuilder to fetch games along with their relations - genres, stores, and parent platforms
-    const queryBuilder = gameRepository
-      .createQueryBuilder("game")
-      .leftJoinAndSelect("game.genres", "genres")
-      .leftJoinAndSelect("game.stores", "stores")
-      .leftJoinAndSelect("game.parent_platforms", "parent_platforms");
+  const { modifiedGames, total } = await getGames(req); // Fetching games using the service layer
 
-    addGenreFilter(queryBuilder, genreId);
-    addStoreFilter(queryBuilder, storeId);
-    addParentPlatformFilter(queryBuilder, parentPlatformId);
-    addOrdering(queryBuilder, ordering);
-
-    const games = await queryBuilder.getMany();
-
-    // Transforming the data to match the expected response structure
-    const modifiedGames: ModifiedGame[] = games.map((game) => ({
-      ...game,
-      parent_platforms: game.parent_platforms?.map((pp) => ({ platform: pp })),
-    }));
-
-    // Constructing the final response object
-    const response: Response = {
-      count: games.length,
-      results: modifiedGames,
-    };
-    res.send(response); // Sending the response back to the client
-  } catch (error) {
-    console.error("Error fetching games:", error);
-    res.status(500).send({ error: "Internal server error" });
-  }
+  // Constructing the final response object
+  const response: Response = buildGamesResponse(modifiedGames, total, req);
+  res.send(response); // Sending the response back to the client
 });
 
 export default gameRouter;

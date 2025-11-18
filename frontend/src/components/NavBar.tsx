a66f835bf025f47ecb1f728e82bd165da68323b2
Madeleine Madsen <mama0002@stud.kea.dk>
2025-11-18 18:08:07 +0100

    Efter undervisningen - Reset all filters when clicking on the logo

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -2,14 +2,16 @@ import { Link } from "react-router-dom";
 import { HStack, Image } from "@chakra-ui/react";
 
 import logo from "../assets/logo.webp";
+import useGameQueryStore from "../store";
 import { ColorModeSwitch } from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
 export const NavBar = () => {
+  const resetGameQuery = useGameQueryStore((s) => s.reset);
   return (
     <HStack justifyContent={"space-between"} padding={4}>
       <Image src={logo} boxSize="60px" />
-      <Link to="/">
+      <Link to="/" onClick={resetGameQuery}>
         <Image src={logo} boxSize="60px" objectFit="cover" />
       </Link>
       <SearchInput />
